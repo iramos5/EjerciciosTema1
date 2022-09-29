d360e289d9046f313c448b7c26b1530b40611df5
iramos5 <ismael.ramos@iesnervion.es>
2022-09-29 19:44:16 +0200

    ejercicio 2

---
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio2.java
@@ -0,0 +1,30 @@
+package ejerciciostema1;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		/* Escribe un programa que tome como entrada un número entero e 
+		 * indique qué cantidad hay que sumarle para que sea múltiplo de 7. 
+		 * Por ejemplo, a 2 hay que sumarle 5 para que sea múltiplo de 7. 
+		 * En el caso de 13 habría que sumarle 1. Usa el operador módulo (%) para calcularlo. */
+
+		// variables
+		Scanner scan = new Scanner(System.in);
+		int numEntero, cantidad, resto;
+		
+		// pedir datos
+		System.out.println("Introduce un número entero: ");
+		numEntero = scan.nextInt();
+		
+		// calcular la cantidad que hay que sumarle para que sea múltiplo de 7
+		resto = numEntero%7;
+		cantidad = 7 - resto;
+		
+		// mostrar solución
+		System.out.println("Para que " + numEntero + " sea múltiplo de 7, hay que sumarle " + cantidad);
+		
+		
+	}
+
+}
