8ce4e94a36834d0d3e616e6951d948cc54d90c7e
iramos5 <ismael.ramos@iesnervion.es>
2022-09-29 20:11:43 +0200

    comentarios

--- EjerciciosTema1/src/ejerciciostema1/Ejercicio2.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio2.java
@@ -10,19 +10,19 @@ public class Ejercicio2 {
 		 * En el caso de 13 habría que sumarle 1. Usa el operador módulo (%) para calcularlo. */
 
 		// variables
-		Scanner scan = new Scanner(System.in);
-		int numEntero, cantidad, resto;
+		Scanner scan = new Scanner(System.in); // se inicia el escáner
+		int numEntero, cantidad, resto; // variables enteras
 		
 		// pedir datos
-		System.out.println("Introduce un número entero: ");
-		numEntero = scan.nextInt();
+		System.out.println("Introduce un número entero: "); // pedir al usuario un numero
+		numEntero = scan.nextInt(); // introducir el numero
 		
 		// calcular la cantidad que hay que sumarle para que sea múltiplo de 7
-		resto = numEntero%7;
-		cantidad = 7 - resto;
+		resto = numEntero%7; // calcular el resto
+		cantidad = 7 - resto; // calcular la cantidad que hay que sumarle al numero
 		
 		// mostrar solución
-		System.out.println("Para que " + numEntero + " sea múltiplo de 7, hay que sumarle " + cantidad);
+		System.out.println("Para que " + numEntero + " sea múltiplo de 7, hay que sumarle " + cantidad); // respuesta que se muestra al usuario
 		
 		// cierre escáner
 		scan.close();
--- EjerciciosTema1/src/ejerciciostema1/Ejercicio4.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio4.java
@@ -7,21 +7,21 @@ public class Ejercicio4 {
 		// Crea un programa que pida la base y la altura de un triángulo y muestre su área. (AT = base*altura/2)
 		
 		// variables
-		Scanner scan = new Scanner(System.in);
-		float base, altura, area;
+		Scanner scan = new Scanner(System.in); // se inicia el escáner
+		float base, altura, area; // variables decimales
 		
 		// pedir datos
-		System.out.println("Introduce la base del triángulo: ");
-		base = scan.nextFloat();
+		System.out.println("Introduce la base del triángulo: "); // se pide la base al usuario
+		base = scan.nextFloat(); // introduccion de la base
 		
-		System.out.println("Introduce la altura del triángulo: ");
-		altura = scan.nextFloat();
+		System.out.println("Introduce la altura del triángulo: "); // se pide la altura al usuario
+		altura = scan.nextFloat(); // introduccion de la altura
 		
 		// calcular area
-		area = base*altura/2;
+		area = base*altura/2; // formula aplicada para calcular el area
 		
 		// solucion
-		System.out.println("El área del triángulo es de: " + area);
+		System.out.println("El área del triángulo es de: " + area); // respuesta que se muestra al usuario
 		
 		// cierre escáner
 		scan.close();
