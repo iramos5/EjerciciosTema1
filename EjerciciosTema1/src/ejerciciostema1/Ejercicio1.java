a81c527f1dadacf739122575a37d3f0fac58d974
iramos5 <ismael.ramos@iesnervion.es>
2022-09-29 19:27:02 +0200

    Ejercicio 1

--- EjerciciosTema1/src/ejerciciostema1/Ejercicio1.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio1.java
@@ -1,10 +1,27 @@
 package ejerciciostema1;
+import java.util.Scanner;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("prueba github");
+		/* Realizar un programa que pida como entrada un número con decimales y 
+		 * lo muestre redondeado al entero más próximo. (SIN UTILIZAR Math.round()) */
+		
+		// variables
+		Scanner scan = new Scanner(System.in);
+		float numDecimal;
+		int numEntero;
+		
+		// pedir datos
+		System.out.println("Introduce un número con decimales: ");
+		numDecimal = scan.nextFloat();
+		
+		// redondear número
+		numEntero = (int) (numDecimal + 0.5);
+		
+		// mostrar número entero redondeado
+		System.out.println("El número redondeado es: " + numEntero);
+		
 	}
 
 }
