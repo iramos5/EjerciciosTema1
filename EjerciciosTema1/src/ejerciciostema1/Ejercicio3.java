64ed5a73c231fff8ad04fb482d0d6f6682b392fa
iramos5 <ismael.ramos@iesnervion.es>
2022-09-29 19:51:04 +0200

    ejercicio 3

--- EjerciciosTema1/src/ejerciciostema1/Ejercicio3.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio3.java
@@ -8,23 +8,23 @@ public class Ejercicio3 {
 		por ejemplo, num1 y num2, diga qué cantidad hay que sumarle a num1 para que sea múltiplo de num2. */
 		
 		// variables
-				Scanner scan = new Scanner(System.in);
-				int num1, num2, resto, cantidad;
-				
-				// pedir datos
-				System.out.println("Introduce el primer número entero: ");
-				num1 = scan.nextInt();
-				
-				System.out.println("Introduce el segundo número entero: ");
-				num2 = scan.nextInt();
-				
-				
-				// calcular la cantidad que hay que sumarle para que num1 sea múltiplo de num2
-				resto = num1%num2;
-				cantidad = num2 - resto;
-				
-				// mostrar solución
-				System.out.println("Para que " + num1 + " sea múltiplo de " + num2 + ", hay que sumarle " + cantidad);
+		Scanner scan = new Scanner(System.in);
+		int num1, num2, resto, cantidad;
+					
+		// pedir datos
+		System.out.println("Introduce el primer número entero: ");
+		num1 = scan.nextInt();
+					
+		System.out.println("Introduce el segundo número entero: ");
+		num2 = scan.nextInt();
+					
+					
+		// calcular la cantidad que hay que sumarle para que num1 sea múltiplo de num2
+		resto = num1%num2;
+		cantidad = num2 - resto;
+					
+		// mostrar solución
+		System.out.println("Para que " + num1 + " sea múltiplo de " + num2 + ", hay que sumarle " + cantidad);
 	}
 
 }
