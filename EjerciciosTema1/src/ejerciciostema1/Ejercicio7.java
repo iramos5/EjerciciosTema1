f8f20988116d93b30b684d58b38cfa60f312ca00
iramos5 <ismael.ramos@iesnervion.es>
2022-09-29 20:48:40 +0200

    enunciado

---
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio7.java
@@ -0,0 +1,17 @@
+package ejerciciostema1;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		/* Solicita al usuario tres distancias:
+		La primera, medida en milímetros.
+		La segunda, medida en centímetros.
+		La última, medida en metros.
+		Diseña un programa que muestre la suma de las tres longitudes introducidas (medida en centímetros).
+		*/
+		
+		
+
+	}
+
+}
