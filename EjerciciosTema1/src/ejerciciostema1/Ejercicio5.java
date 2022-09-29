295a99531fdcf6069a6e496fa2febe5e7adcb3ad
iramos5 <ismael.ramos@iesnervion.es>
2022-09-29 20:28:22 +0200

    ejercicio 5

--- EjerciciosTema1/src/ejerciciostema1/Ejercicio5.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio5.java
@@ -1,4 +1,5 @@
 package ejerciciostema1;
+import java.util.Scanner;
 
 public class Ejercicio5 {
 
@@ -8,6 +9,31 @@ public class Ejercicio5 {
 			Crea un programa que pida los coeficientes a, b y c, 
 			así como el valor de x, y calcula el valor correspondiente de y. 
 		*/
+		
+		// variables
+		Scanner scan = new Scanner(System.in); // iniciar escáner
+		int a, b, c, x, y; // variables que se piden
+		
+		// pedir datos al usuario
+		System.out.println("Introduce a: "); // pedir al usuario a
+		a = scan.nextInt(); // guardar a
+		
+		System.out.println("Introduce b: "); // pedir al usuario b
+		b = scan.nextInt(); // guardar b
+		
+		System.out.println("Introduce c: "); // pedir al usuario c
+		c = scan.nextInt(); // guardar c
+		
+		System.out.println("Introduce x: "); // pedir al usuario x
+		x = scan.nextInt(); // guardar x
+		
+		// operaciones
+		y = (int) ((a * Math.pow(x, 2)) + (b * x) + c); // aplicar la formula y aplicar cast por la x elevado a 2
+		
+		// resultado
+		System.out.println("El valor de y es: " + y); // imprimir resultado
+		
+		
 	}
 
 }
