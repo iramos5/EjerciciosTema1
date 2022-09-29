1ff2f11e6c93cc76f3af3c141bd72c9a721c6048
iramos5 <ismael.ramos@iesnervion.es>
2022-09-29 20:06:45 +0200

    comentarios

--- EjerciciosTema1/src/ejerciciostema1/Ejercicio1.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio1.java
@@ -8,19 +8,19 @@ public class Ejercicio1 {
 		 * lo muestre redondeado al entero más próximo. (SIN UTILIZAR Math.round()) */
 		
 		// variables
-		Scanner scan = new Scanner(System.in);
-		float numDecimal;
-		int numEntero;
+		Scanner scan = new Scanner(System.in); // se inicia el escáner
+		float numDecimal; // variable del número de entrada
+		int numEntero; // variable del número entero final
 		
 		// pedir datos
-		System.out.println("Introduce un número con decimales: ");
-		numDecimal = scan.nextFloat();
+		System.out.println("Introduce un número con decimales: "); // mensaje para el usuario
+		numDecimal = scan.nextFloat(); // introduccion del número por teclado
 		
 		// redondear número
-		numEntero = (int) (numDecimal + 0.5);
+		numEntero = (int) (numDecimal + 0.5); // redondeo del número decimal a uno entero
 		
 		// mostrar número entero redondeado
-		System.out.println("El número redondeado es: " + numEntero);
+		System.out.println("El número redondeado es: " + numEntero); // respuesta que se muestra al usuario
 		
 		// cierre escáner
 		scan.close();
