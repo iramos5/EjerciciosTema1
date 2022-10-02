c078b98a50fa32ebc7d0b3abddb1c09179b5b978
iramos5 <ismael.ramos@iesnervion.es>
2022-10-02 15:22:48 +0200

    ejercicio 7

--- EjerciciosTema1/src/ejerciciostema1/Ejercicio7.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio7.java
@@ -1,4 +1,5 @@
 package ejerciciostema1;
+import java.util.Scanner;
 
 public class Ejercicio7 {
 
@@ -10,7 +11,30 @@ public class Ejercicio7 {
 		Diseña un programa que muestre la suma de las tres longitudes introducidas (medida en centímetros).
 		*/
 		
+		// variables
+		Scanner scan = new Scanner(System.in); // iniciar escáner
+		float mm, cm, m, suma; // variables
 		
+		// introducir datos
+		System.out.println("Introduce una cantidad de milímetros: "); // pedir al usuario los milímetros
+		mm = scan.nextFloat(); // guardar milímetros
+		
+		System.out.println("Introduce una cantidad de centímetros: "); // pedir al usuario los centímetros
+		cm = scan.nextFloat(); // guardar centímetros
+		
+		System.out.println("Introduce una cantidad de metros: "); // pedir al usuario los metros
+		m = scan.nextFloat(); // guardar metros
+		
+		// cálculos
+		mm = mm/10; // pasar los milímetros a centímetros
+		m = m * 100; // pasar los metros a centímetros
+		suma = mm + cm + m; // sumar todas las cantidades convertidas
+		
+		// solución
+		System.out.println("La suma de las tres cantidades equivale a: " + suma + " centímetros"); // resultado por pantalla
+			
+		// cierre escáner
+		scan.close();
 
 	}
 
